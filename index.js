8f4f2e30ccc1e547bfbe03812ebe70bd96bb107b
kemmie <kemmie.xd@gmail.com>
2018-10-22 22:14:32 +0700

    22h 22/10

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 var userRoute = require('./routes/users.route');
+var authRoute = require('./routes/auth.route');
 
+var authMiddleware = require('./middlewares/auth.middleware');
 
 var db = require('./db');
 
@@ -14,13 +17,14 @@ app.set('view engine', 'pug');
 app.set('views', './views');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
+app.use(cookieParser());
 
 app.use(express.static('publics'));
 
 
 // Routes
-app.use('/users', userRoute);
+app.use('/users', authMiddleware.requireAuth, userRoute);
+app.use('/auth', authRoute);
 
 app.get('/', function(req, res) {
     res.render('index', {
--- routes/users.route.js
+++ routes/users.route.js
@@ -2,10 +2,16 @@ var express = require('express');
 
 var controller = require('../controller/users.controller');
 var validate = require('../validate/users.validate');
+var authMiddleware = require('../middlewares/auth.middleware');
 
 var router = express.Router();
 
-router.get('/', controller.index);
+router.get('/', authMiddleware.requireAuth, controller.index);
+
+router.get('/cookie', function(req, res, next) {
+    res.cookie('name-id', 12345);
+    res.send('Helo Madu Phuc')
+});
 
 router.get('/search', controller.search);
 
